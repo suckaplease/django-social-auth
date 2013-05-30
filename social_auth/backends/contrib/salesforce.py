234a16d710c4722aea7d5003a0dbd1e30cd455a1
A.T. Fouty <afouty@gmail.com>
2013-05-30 10:44:35 -0700

    added salesforce backend

---
+++ social_auth/backends/contrib/salesforce.py
@@ -0,0 +1,105 @@
+"""
+settings.py should include the following:
+
+    SALESFORCE_CLIENT_ID = '...'
+    SALESFORCE_CLIENT_SECRET = '...'
+
+Optional scope to include:
+    api - Allows access to the current, logged-in user’s account over the APIs, such as the REST API or Bulk API.
+    chatter_api - Allows access to only the Chatter API URLs.
+    full - Allows access to all data accessible by the current, logged-in user.
+    id - Allows access only to the Identity Service.
+    refresh_token - Allows a refresh token to be returned if you are eligible to receive one.
+    visualforce - Allows access to Visualforce pages.
+    web - Allows the ability to use the access_token on the Web.
+
+    If you do not supply a scope parameter, it will default to: id api refresh_token.
+
+    SALESFORCE_AUTH_EXTRA_ARGUMENTS = {'scope': 'id api refresh_token'}
+
+    SALESFORCE_DISPLAY_PARAM
+
+More information on scope can be found at https://angel.co/api/oauth/faq
+"""
+from urllib import urlencode
+
+from django.utils import simplejson
+
+from social_auth.backends import BaseOAuth2, OAuthBackend
+from social_auth.utils import dsa_urlopen, setting
+
+from oauth2 import Token
+
+
+SALESFORCE_DOMAIN = 'login.salesforce.com'
+SALESFORCE_TEST_DOMAIN = 'test.salesforce.com'
+
+SALESFORCE_TESTING = setting('SALESFORCE_TESTING',False)
+SALESFORCE_SERVER = "https://" + SALESFORCE_TEST_DOMAIN if SALESFORCE_TESTING else SALESFORCE_DOMAIN
+
+SALESFORCE_AUTHORIZATION_PATH = '/services/oauth2/authorize'
+SALESFORCE_ACCESS_TOKEN_PATH = '/services/oauth2/token'
+
+
+SALESFORCE_AUTHORIZATION_URL = SALESFORCE_SERVER + SALESFORCE_AUTHORIZATION_PATH
+SALESFORCE_ACCESS_TOKEN_URL = SALESFORCE_SERVER + SALESFORCE_ACCESS_TOKEN_PATH
+
+class SalesforceBackend(OAuthBackend):
+    name = 'salesforce'
+
+    EXTRA_DATA = [
+        ('user_id', 'user_id'),
+        ('asserted_user', 'asserted_user'),
+        ('organization_id','organization_id'),
+        ('username','username'),
+        ('display_name', 'display_name'),
+        ('email', 'email'),
+        ('status','status'),
+        ('photos','photos'),
+        ('urls','urls'),
+    ]
+
+    def get_user_id(self, details, response):
+        return response['user_id']
+
+    def get_user_details(self, response):
+        """Return user details from Salesforce account"""
+        username = response['username']
+        first_name = response['display_name'].split(' ')[0]
+        last_name = response['display_name'].split(' ')[-1]
+        email = response['email']
+        return {
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+        }
+
+
+class SalesforceAuth(BaseOAuth2):
+    """Salesforce OAuth mechanism"""
+    AUTHORIZATION_URL = SALESFORCE_AUTHORIZATION_URL
+    ACCESS_TOKEN_URL = SALESFORCE_ACCESS_TOKEN_URL
+    AUTH_BACKEND = SalesforceBackend
+    SETTINGS_KEY_NAME = 'SALESFORCE_CLIENT_ID'
+    SETTINGS_SECRET_NAME = 'SALESFORCE_CLIENT_SECRET'
+    # REDIRECT_STATE = False
+    # STATE_PARAMETER = False
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        response = kwargs.get('response') or {}
+
+
+        params = {'access_token': access_token}
+        url = response.get('id') + '?' + urlencode(params)
+        try:
+            return simplejson.load(dsa_urlopen(url))
+        except ValueError:
+            return None
+
+
+# Backend definition
+BACKENDS = {
+    'salesforce': SalesforceAuth,
+}
